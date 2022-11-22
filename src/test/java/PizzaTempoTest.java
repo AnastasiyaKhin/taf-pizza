9f46442cb1e610dbc7247641096298f1edea9611
Anastasiya Khinevich <xinevich.av@gmail.com>
2022-11-22 15:43:14 +0100

    Initial commit

---
+++ src/test/java/PizzaTempoTest.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+public class PizzaTempoTest extends BaseTest {
+
+    @Test
+    void testOrderPizzaMargarita(){
+
+        PizzaTempoPage page = new PizzaTempoPage(driver);
+
+        page.openPizzaTempoPage();
+        page.clickPizzaChoiceLink();
+        page.clickPizzaMargaritaButton();
+        page.clickPizzaMargaritaChoiceDough();
+        page.clickPizzaMargaritaChoiceSize();
+        page.clickPizzaMargaritaOrderButton();
+        page.clickDrinkChoiceLink();
+        page.clickDrinkSpriteButton();
+        page.clickPizzaBasketLink();
+
+        Assertions.assertEquals(page.EXPECTED_RESULT_ORDER_PIZZA,page.getPizzaOrderText());
+        Assertions.assertEquals(page.EXPECTED_RESULT_ORDER_DRINK,page.getDrinkOrderText());
+    }
+}
